de2aabdb2ee4891072600297201e89a5d2304fb9
TheodorosPloumis <me@theodorosploumis.com>
2017-10-21 16:04:41 +0300

    Fix typo on GA code

--- html/index.php
+++ html/index.php
@@ -40,7 +40,7 @@ $images = [
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', <?php echo $google_analytics_code; ?>);
+      gtag('config', '<?php echo $google_analytics_code; ?>');
     </script>
 
 </head>
