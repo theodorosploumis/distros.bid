472ae070a3208df624b45a304412956d8922b7bb
TheodorosPloumis <me@theodorosploumis.com>
2017-10-21 14:38:41 +0300

    Add extra tools on server install

--- scripts/server-install.sh
+++ scripts/server-install.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 # Custom script to install software on the server. Run with 'sudo'.
+# Change variables according to your neeeds
 
 DOMAIN="distros.bid"
 SUBDOMAIN="drupal.distros.bid"
+
+INSTALL_RANCHER=1
+INSTALL_PORTAINER=1
+INSTALL_CADVISOR=1
+
 NGINXPORT="8055"
 PORTAINERPORT="9988"
 RANCHERPORT="9989"
+CADVISORPORT="9990"
 
 # Generic software
 apt-get -qqy update
@@ -25,20 +32,46 @@ git clone https://github.com/theodorosploumis/drupal-docker-distros.git /var/www
 curl https://releases.rancher.com/install-docker/17.06.sh | sh
 
 # Start nginx-proxy on port $NGINXPORT
-docker run -d -p ${NGINXPORT}:80 --name proxy \
-       -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy
+docker run -d -p ${NGINXPORT}:80 \
+       --name proxy \
+       --restart=unless-stopped \
+       -v /var/run/docker.sock:/tmp/docker.sock:ro \
+       jwilder/nginx-proxy
 
 # Add www-data to group docker
 usermod -aG docker www-data
 
 # Start Portainer dashboard
+if [ "${INSTALL_PORTAINER}" -eq "1" ]; then
 docker volume create portainer_data
-docker run -d -p ${PORTAINERPORT}:9000 -v /var/run/docker.sock:/var/run/docker.sock \
-       -v portainer_data:/data portainer/portainer
+docker run -d -p ${PORTAINERPORT}:9000 \
+       -v /var/run/docker.sock:/var/run/docker.sock \
+       -v portainer_data:/data \
+       --name=portainer \
+       portainer/portainer
+fi
 
 # Start rancher dashboard
-docker run -d --name rancher_server --restart=unless-stopped \
-       -p ${RANCHERPORT}:8080 rancher/server:stable
+if [ "${INSTALL_RANCHER}" -eq "1" ]; then
+docker run -d --name=rancher_server \
+       --restart=unless-stopped \
+       -p ${RANCHERPORT}:8080 \
+       rancher/server:stable
+fi
+
+# Start cAdvisor dashboard
+if [ "${INSTALL_CADVISOR}" -eq "1" ]; then
+   docker run \
+          --volume=/:/rootfs:ro \
+          --volume=/var/run:/var/run:rw \
+          --volume=/sys:/sys:ro \
+          --volume=/var/lib/docker/:/var/lib/docker:ro \
+          --volume=/dev/disk/:/dev/disk:ro \
+          --publish=${CADVISORPORT}:8080 \
+          --detach=true \
+          --name=cadvisor \
+          google/cadvisor:latest
+fi
 
 # Install php packages
 cd /var/www/distros/html && \
@@ -55,6 +88,11 @@ curl -sSL https://agent.digitalocean.com/install.sh | sh
 
 # Pull all docker images
 bash /var/www/distros/scripts/pull-images.sh
+
+# Link extra aliases
+touch ~/.bashrc
+echo -e "if [ -f /var/www/distros/scripts/.docker-aliases ]; then . /var/www/distros/scripts/.docker-aliases fi" >> ~/.bashrc
+source ~/.bashrc
 
 # Remove unused packages
 apt-get autoremove
