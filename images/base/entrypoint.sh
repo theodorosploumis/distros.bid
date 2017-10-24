09c86fe6800f4ef97a50c2be1ebc0e65db72a8b7
TheodorosPloumis <me@theodorosploumis.com>
2017-10-25 02:32:13 +0300

    Fix entrypoint

--- images/base/entrypoint.sh
+++ images/base/entrypoint.sh
@@ -1,7 +1,31 @@
 #!/usr/bin/env bash
 
-# Run MariaDB
-service mysql start
+function start_all() {
+  # Execute configuration script
+  /root/configure.py
 
-# Run Apache:
-service apache2 start
+  # Start services
+  /etc/init.d/mysql start
+  /etc/init.d/apache2 start
+  cron
+
+  # Expose a bash script for interactive mode
+  /bin/bash
+}
+
+function reload_all() {
+  echo "Reloading..."
+  /etc/init.d/mysql restart
+  /etc/init.d/apache2 restart
+}
+
+function stop_all() {
+  echo "Shutting down..."
+  /etc/init.d/mysql stop
+  /etc/init.d/apache2 stop
+}
+
+trap stop_all EXIT
+trap reload_all SIGHUP
+
+start_all
