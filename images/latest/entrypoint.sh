8f9f18505e6f5c99cdb56a4048f4826e5e6bb6fb
TheodorosPloumis <me@theodorosploumis.com>
2017-10-25 17:05:11 +0300

    Entrypoint: start mysql before apache2

--- images/latest/entrypoint.sh
+++ images/latest/entrypoint.sh
@@ -3,7 +3,9 @@
 #/etc/init.d/mysql start
 #/etc/init.d/apache2 start
 
-chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && /usr/bin/mysqld_safe
+# chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && /usr/bin/mysqld_safe
+
+service mysql start
 
 /usr/sbin/apache2ctl start
 
