57a58c4e6bbe66b069b2392043f367056831e7cf
TheodorosPloumis <me@theodorosploumis.com>
2017-10-25 18:07:22 +0300

    Fix entrypoint

--- images/latest/entrypoint.sh
+++ images/latest/entrypoint.sh
@@ -2,10 +2,9 @@
 
 #/etc/init.d/mysql start
 #/etc/init.d/apache2 start
+#/usr/bin/mysqld_safe
 
-# chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && /usr/bin/mysqld_safe
-
-service mysql start
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld && service mysql start
 
 /usr/sbin/apache2ctl start
 
