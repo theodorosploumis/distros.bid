c846f3382ad259f76f3568a5e20496cadf24acb9
TheodorosPloumis <me@theodorosploumis.com>
2017-12-12 12:24:31 +0200

    Use favicon.ico file

--- html/index.php
+++ html/index.php
@@ -22,7 +22,7 @@ require_once __DIR__ . '/distributions.php';
 
     <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Lato|Source+Sans+Pro:300,400" rel="stylesheet">
-    <link rel="icon" type="image/png" sizes="20x20" href="favicon.png">
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon" />
 
     <?php print googleOptimize($google_optimize_code); ?>
     <?php print googleAnalyticsOptimize($google_analytics_code, $google_optimize_code); ?>
