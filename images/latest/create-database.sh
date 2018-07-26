771276c81d9deb637fc33f00c7b828db29ff7fa7
TheodorosPloumis <me@theodorosploumis.com>
2018-07-26 20:21:18 +0300

    Install db on each image build

--- images/latest/create-database.sh
+++ images/latest/create-database.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
-/usr/bin/mysqld_safe > /dev/null 2>&1 &
-PID=$!
-RET=1
+# /usr/bin/mysqld_safe > /dev/null 2>&1 &
+# PID=$!
+# RET=1
 
-while [[ $RET -ne 0 ]]; do
-    echo "=> Waiting for confirmation of MySQL service startup"
-    sleep 5
-    mysql -u root -e "status" > /dev/null 2>&1
-    RET=$?
-done
+# while [[ $RET -ne 0 ]]; do
+#     echo "=> Waiting for confirmation of MySQL service startup"
+#     sleep 5
+#     mysql -u root -e "status" > /dev/null 2>&1
+#     RET=$?
+# done
 
-echo "=> Started with PID ${PID}"
+# echo "=> Started with PID ${PID}"
 
 mysql -u root -e "CREATE DATABASE drupal CHARACTER SET utf8 COLLATE utf8_general_ci" && \
 mysql -u root -e "GRANT ALL ON drupal.* to 'drupal' identified by 'drupal';"
-
-echo "=> Stopping MySQL Server"
-service mysql stop
 
 echo "=> Done!"
