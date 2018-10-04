d0cf389a087350cf28282a1e59718d2697b29c84
TheodorosPloumis <me@theodorosploumis.com>
2018-10-04 22:18:37 +0300

    Update latest to allow sqlite db

---
+++ images/latest/install-sqlite.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Install a Drupal site with Drush
+
+if [ -z ${DOCROOT} ]
+  then
+    DOCROOT="/var/www/html";
+fi
+
+# Install site
+service apache2 start && \
+drush site-install -y ${PROFILE} \
+      --site-name="Distribution ${NEWDISTRO} with Docker" \
+      --db-url=sqlite://sites/default/files/.ht.sqlite \
+      --site-mail=admin@example.com \
+      --account-name=admin \
+      --account-pass=admin \
+      --account-mail=admin@example.com
+
+# Change site name
+drush config-set system.site name "Drupal version: $(drush pmi --fields=Version system | sed 's/\ Version   :  //g') - Installation profile: ${NEWDISTRO}" -y
+drush variable-set site_name "Drupal version: $(drush pmi --fields=Version system | sed 's/\ Version   :  //g') - Installation profile: ${NEWDISTRO}" -y
+
+# Assign all site files to www-data
+chown -R www-data:www-data /var/www/html
+
+# Move terminal.php to docroot
+cp /var/www/terminal.php ${DOCROOT}/terminal.php
