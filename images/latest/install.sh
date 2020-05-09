1cd34816caf42c87f7dc53bf51f74f06ee390957
Theodoros Ploumis <me@theodorosploumis.com>
2020-05-09 15:51:12 +0300

    Add composer.json as copied file

--- images/latest/install.sh
+++ images/latest/install.sh
@@ -7,9 +7,9 @@ if [ -z ${DOCROOT} ]
 fi
 
 # Create settings.php file
-echo -e ">> Create settings.php file"
-cp ${DOCROOT}/sites/default/default.settings.php ${DOCROOT}/sites/default/settings.php
-chmod 777 ${DOCROOT}/sites/default/settings.php
+#echo -e ">> Create settings.php file"
+#cp ${DOCROOT}/sites/default/default.settings.php ${DOCROOT}/sites/default/settings.php
+#chmod 777 ${DOCROOT}/sites/default/settings.php
 
 # Alter settings.php
 echo "\$settings['trusted_host_patterns'] = array('.',);" >> ${DOCROOT}/sites/default/settings.php && \
@@ -19,31 +19,31 @@ echo "ini_set('max_execution_time', '1200');" >> ${DOCROOT}/sites/default/settin
 echo "\$settings['file_private_path'] = '${DOCROOT}/sites/default/private-files';" >> ${DOCROOT}/sites/default/settings.php
 
 # Create public files folder
-echo -e ">> Create public, private files and libraries folders"
-if [ ! -d ${DOCROOT}/sites/default/files ]
-  then
-    mkdir -p ${DOCROOT}/sites/default/files;
-fi
-
-if [ ! -d ${DOCROOT}/libraries ]
-  then
-    mkdir ${DOCROOT}/libraries;
-fi
+#echo -e ">> Create public, private files and libraries folders"
+#if [ ! -d ${DOCROOT}/sites/default/files ]
+#  then
+#    mkdir -p ${DOCROOT}/sites/default/files;
+#fi
+#
+#if [ ! -d ${DOCROOT}/libraries ]
+#  then
+#    mkdir ${DOCROOT}/libraries;
+#fi
 
 mkdir ${DOCROOT}/sites/default/private-files
 
 # Change owner and mode of public files
-echo -e ">> Change owner and mode of public files"
-chown -R www-data:www-data ${DOCROOT}/sites/default
-chmod -R 777 ${DOCROOT}/sites/default/files
-chmod -R 777 ${DOCROOT}/libraries
-chmod 644 ${DOCROOT}/sites/default/default.settings.php
-chmod 644 ${DOCROOT}/sites/default/default.services.yml
+#echo -e ">> Change owner and mode of public files"
+#chown -R www-data:www-data ${DOCROOT}/sites/default
+#chmod -R 777 ${DOCROOT}/sites/default/files
+#chmod -R 777 ${DOCROOT}/libraries
+#chmod 644 ${DOCROOT}/sites/default/default.settings.php
+#chmod 644 ${DOCROOT}/sites/default/default.services.yml
 
 echo -e ">> Drupal is ready for installation."
 
 # 2. Install a Drupal site with Drush
-service apache2 start && \
+#service apache2 start && \
 vendor/bin/drush site-install -y ${PROFILE} \
       --site-name="${DISTRO} by Distros.bid" \
       --db-url=sqlite://sites/default/files/.ht.sqlite \
@@ -53,8 +53,7 @@ vendor/bin/drush site-install -y ${PROFILE} \
       --account-mail=admin@example.com
 
 # Change site name
-vendor/bin/drush config-set system.site name "Drupal: $(vendor/bin/drush pmi --fields=Version system | sed 's/\ Version   :  //g') - Profile: ${DISTRO}" -y
-vendor/bin/drush variable-set site_name "Drupal: $(vendor/bin/drush pmi --fields=Version system | sed 's/\ Version   :  //g') - Profile: ${DISTRO}" -y
+vendor/bin/drush config-set system.site name "${DISTRO}-${VERSION}" -y
 
 # Assign all site files to www-data
 chown -R www-data:www-data /var/www/html
