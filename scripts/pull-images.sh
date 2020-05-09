716a3d5b632ce5dc2fb445d31ba2eb68d63e2482
TheodorosPloumis <me@theodorosploumis.com>
2020-05-09 20:10:04 +0300

    Update distros Dockerfiles

--- scripts/pull-images.sh
+++ scripts/pull-images.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 # Pull all docker images of this repo
 
-cat $DIR/../tags8.txt | while read line
+cat $DIR/../tags.txt | while read line
 do
-  ID=$(echo "$line" | cut -c1-2)
-  DISTRO=$(echo $line | sed -r 's/^.{3}//')
-
-  echo "$ID. Pulling image" $DISTRO;
-  docker pull drupal8/distros:$DISTRO;
+  echo "Pulling image" $line;
+  docker pull drupal8/distros:$line;
 done
