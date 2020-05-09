6506bbe6f1e65e119fe10845dc9f742a160a2379
Theodoros Ploumis <me@theodorosploumis.com>
2020-05-09 13:39:16 +0300

    Use ubuntu 20.04 base image

--- images/latest/install.sh
+++ images/latest/install.sh
@@ -1,29 +1,73 @@
 #!/usr/bin/env bash
-# Install a Drupal site with Drush
+# 1. Prepare a Drupal site for installation
 
 if [ -z ${DOCROOT} ]
   then
     DOCROOT="/var/www/html";
 fi
 
+# Create settings.php file
+echo -e ">> Create settings.php file"
+cp ${DOCROOT}/sites/default/default.settings.php ${DOCROOT}/sites/default/settings.php
+chmod 777 ${DOCROOT}/sites/default/settings.php
+
+# Alter settings.php
+echo "\$settings['trusted_host_patterns'] = array('.',);" >> ${DOCROOT}/sites/default/settings.php && \
+echo "\$config_directories = array(CONFIG_SYNC_DIRECTORY => '/var/www/config/sync',);" >> ${DOCROOT}/sites/default/settings.php && \
+echo "ini_set('memory_limit', '256M');" >> ${DOCROOT}/sites/default/settings.php && \
+echo "ini_set('max_execution_time', '1200');" >> ${DOCROOT}/sites/default/settings.php && \
+echo "\$settings['file_private_path'] = '${DOCROOT}/sites/default/private-files';" >> ${DOCROOT}/sites/default/settings.php
+
+# Create public files folder
+echo -e ">> Create public, private files and libraries folders"
+if [ ! -d ${DOCROOT}/sites/default/files ]
+  then
+    mkdir -p ${DOCROOT}/sites/default/files;
+fi
+
+if [ ! -d ${DOCROOT}/libraries ]
+  then
+    mkdir ${DOCROOT}/libraries;
+fi
+
+mkdir ${DOCROOT}/sites/default/private-files
+
+# Change owner and mode of public files
+echo -e ">> Change owner and mode of public files"
+chown -R www-data:www-data ${DOCROOT}/sites/default
+chmod -R 777 ${DOCROOT}/sites/default/files
+chmod -R 777 ${DOCROOT}/libraries
+chmod 644 ${DOCROOT}/sites/default/default.settings.php
+chmod 644 ${DOCROOT}/sites/default/default.services.yml
+
+echo -e ">> Drupal is ready for installation."
+
+
+# 2. Install a Drupal site with Drush
+if [ -z ${DOCROOT} ]
+  then
+    DOCROOT="/var/www/html";
+fi
+
 # Install site
-service mysql start && \
 service apache2 start && \
 drush site-install -y ${PROFILE} \
-      --site-name="Distribution ${NEWDISTRO} with Docker" \
-      --db-url=mysql://drupal:drupal@localhost/drupal \
+      --site-name="${NEWDISTRO} by Distros.bid" \
+      --db-url=sqlite://sites/default/files/.ht.sqlite \
       --site-mail=admin@example.com \
       --account-name=admin \
       --account-pass=admin \
       --account-mail=admin@example.com
 
 # Change site name
-service mysql start && \
-drush config-set system.site name "Drupal version: $(drush pmi --fields=Version system | sed 's/\ Version   :  //g') - Installation profile: ${NEWDISTRO}" -y
-drush variable-set site_name "Drupal version: $(drush pmi --fields=Version system | sed 's/\ Version   :  //g') - Installation profile: ${NEWDISTRO}" -y
+drush config-set system.site name "Drupal: $(drush pmi --fields=Version system | sed 's/\ Version   :  //g') - Profile: ${NEWDISTRO}" -y
+drush variable-set site_name "Drupal: $(drush pmi --fields=Version system | sed 's/\ Version   :  //g') - Profile: ${NEWDISTRO}" -y
 
 # Assign all site files to www-data
 chown -R www-data:www-data /var/www/html
 
 # Move terminal.php to docroot
 cp /var/www/terminal.php ${DOCROOT}/terminal.php
+
+# Move SQLite database manager to docroot
+cp /var/www/database.php ${DOCROOT}/database.php
