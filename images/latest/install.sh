cd3f651f09d78ec792ae37b5d685b213a55bfcba
Theodoros Ploumis <me@theodorosploumis.com>
2020-05-09 13:58:42 +0300

    Install drush before installing drupal

--- images/latest/install.sh
+++ images/latest/install.sh
@@ -42,14 +42,10 @@ chmod 644 ${DOCROOT}/sites/default/default.services.yml
 
 echo -e ">> Drupal is ready for installation."
 
+# 2. Install drush
+RUN COMPOSER=composer.json composer require drush/drush:10.2.2 --quiet --no-ansi --no-interaction --no-progress
 
-# 2. Install a Drupal site with Drush
-if [ -z ${DOCROOT} ]
-  then
-    DOCROOT="/var/www/html";
-fi
-
-# Install site
+# 3. Install a Drupal site with Drush
 service apache2 start && \
 drush site-install -y ${PROFILE} \
       --site-name="${NEWDISTRO} by Distros.bid" \
